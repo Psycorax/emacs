587641d4fe1596189d69776d3eb52cf855d21fa9
psycorax <reisinger.thre@gmail.com>
2018-07-21 16:47:38 +0200

    added line numbers to emacs

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
+  int h = 0;
   if (true) {
     cout << "HI" << endl;
+    cout << "This seems to be a dream" << h << endl;
   }
   return 0;
 }
