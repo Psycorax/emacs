9b47c5974d751ffbf1d1dc98d947add74435adcd
psycorax <reisinger.thre@gmail.com>
2018-08-15 22:53:51 +0200

    actualized init.el file

--- init.el
+++ init.el
@@ -1,23 +1,19 @@
-
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives
 	     '("org" . "https://orgmode.org/elpa/") t)
 (package-initialize)
 
-;; Bootstrap 'use-package'
-;; use-package options:
-;; :ensure = ???
-;; :diminish = ???
-;; :bind = used to bind shortcuts(keybindings)(could as well been done in ":config" by global-set-key
-;; :init = executed before the package is loaded
-;; :config = executed after the package is loaded
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(org-babel-load-file (expand-file-name "C:/git/emacs/myinit.org"))
+(use-package org
+       :ensure org-plus-contrib
+       :pin org)
 
+(org-babel-load-file (expand-file-name "~/Documents/git/emacs/myinit_coding.org"))
